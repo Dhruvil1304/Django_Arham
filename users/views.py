b52aae853f7e73f3703139a272aaaba22bbb123a
viralsir <viralsir2018@gmail.com>
2020-11-26 18:22:35 +0530

    login and profile functionality added.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 from django.contrib import messages
+
 
 # Create your views here.
 def register(request):
@@ -10,8 +11,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data["username"]
-            messages.success(request,f"{username} is successfully created.. ")
-            return redirect('blog-home')
+            messages.success(request,f"{username} is successfully created..now you can log in ")
+            return redirect('login')
         else:
             return render(request, "users/register.html", {
                 "form": form
@@ -21,3 +22,7 @@ def register(request):
     return render(request,"users/register.html",{
         "form":form
     })
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request,"users/account.html")
