6da5b7a2f203d3f3dc197762b7261da6b9560ab1
viralsir <viralsir2018@gmail.com>
2020-12-04 17:15:56 +0530

    student app is added

--- flights/selenium_test.py
+++ flights/selenium_test.py
@@ -1,42 +1,8 @@
 from unittest import  TestCase
 from selenium import webdriver
 
-#driver=webdriver.Chrome()
-
-# driver.get("http://127.0.0.1:8000/flights/counter")
-# print(driver.title)
-# print(driver.page_source)
-# increase=driver.find_element_by_id("increase")
-#
-# increase.click()
-# increase.click()
-# increase.click()
-# increase.click()
-# increase.click()
-#
-# for i in range(25):
-#     increase.click()
-#
-#
-# decrease=driver.find_element_by_id("decrease")
-#
-# decrease.click()
-# decrease.click()
-# decrease.click()
-# decrease.click()
-# decrease.click()
-#
-# for i in range(25):
-#     decrease.click()
-
-
-def is_button():
-    return True;
-
-
-
+driver=webdriver.Chrome()
 class testcounterweb(TestCase):
-
 
     def test_increase_button(self):
         driver = webdriver.Chrome()
